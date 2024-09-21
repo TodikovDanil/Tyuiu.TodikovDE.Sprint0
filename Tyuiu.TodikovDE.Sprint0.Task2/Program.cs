aa1ddf7b338868834a1713532fdea2601968eb18
TodikovDanil <todic.danil@gmail.com>
2024-09-21 17:44:47 +0300

    task2

---
+++ Tyuiu.TodikovDE.Sprint0.Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.TodikovDE.Sprint0.Task2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Даниил";
+            int age = 18;
+           Console.WriteLine("Здравствуйте, меня зовут "+ name +  " мне "+ age +" лет ");
+            Console.ReadKey();
+        }
+    }
+}
