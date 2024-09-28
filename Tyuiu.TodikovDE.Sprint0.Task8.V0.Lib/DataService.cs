52474db5b2a5c2f60586492160fae8a7a0d2d9db
TodikovDanil <todic.danil@gmail.com>
2024-09-29 01:26:48 +0300

    Task8

---
+++ Tyuiu.TodikovDE.Sprint0.Task8.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.TodikovDE.Sprint0.Task8.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+}
