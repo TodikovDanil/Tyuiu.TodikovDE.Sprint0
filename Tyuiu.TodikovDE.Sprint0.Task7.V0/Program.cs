0e40805ab0d2218884bfb357f25133291c3050eb
TodikovDanil <todic.danil@gmail.com>
2024-09-28 23:52:11 +0300

    Task7

---
+++ Tyuiu.TodikovDE.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.TodikovDE.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.TodikovDE.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элемнтов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+            Console.WriteLine("Умножение элементов массива = " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
